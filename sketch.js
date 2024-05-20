9b91d7ab621b30e52c6ed71b3ee6fde47430e49e
Buhmend161006 <00001237048618sp@al.educacao.sp.gov.br>
2024-05-20 17:58:51 -0300

    adicionando programa

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(400, 400);
+    background("black")
+  }
+  
+  function draw() {
+    stroke("gray")
+    fill("white")
+    
+    console.log//(mouseIsPressed);
+    rect(mouseX, mouseY, 25, 25);
+  
+    
+  }
